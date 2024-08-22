46bca4f31a2fcaf297fbef36f3964cad16055221
mThunbo <mthunbov2@gmail.com>
2024-08-22 10:47:51 +0200

    refined the meny and subtraction functions

--- GoLangProject/main.go
+++ GoLangProject/main.go
@@ -7,37 +7,65 @@ import (
 	"runtime"
 )
 
+// Addition function that returns the sum of a given number of values
 func addition(x ...float64) float64 {
-	sum := 0.0
+	if len(x) == 0 {
+		return 0.0
+	}
+	var sum float64
 	for _, i := range x {
 		sum += i
 	}
 	return sum
 }
 
+// Subtraction functions that takes an arbitrary number of values adn subtracts from the first given value
 func subtraction(x ...float64) float64 {
-	sum := 0.0
-	for _, i := range x {
+	if len(x) == 0 {
+		return 0.0
+	}
+	sum := x[0]
+	for _, i := range x[1:] {
 		sum -= i
 	}
 	return sum
 }
 
+// Square root function that returns the square root of a given number
 func Sqrt(x float64) float64 {
-	z := x / 2
-	for z1 := 0.0; z1 != z; {
-		z1 = z
-		z -= (z*z - x) / (2 * z)
+	sum := x / 2
+	for i := 0.0; i != sum; {
+		i = sum
+		sum -= (sum*sum - x) / (2 * sum)
 	}
-	return z
+	return sum
 }
 
+// Gets user input and performs the corresponding calculations with it
+func getUserInput(choice int) []float64 {
+	var opscount int
+	values := []float64{}
+	operations := [5]string{"ERROR!!!", "Addition", "Subtraction", "Multiplication", "Division"}
+	operands := [5]string{"ERROR!!!", "add", "subtract", "multiply", "divide"}
+
+	clearTerminal()
+	fmt.Printf("---%s---\nPlease input the amount numbers you want to %s:", operations[choice], operands[choice])
+	fmt.Scan(&opscount)
+	fmt.Printf("Please input the numbers to %s together, press enter after each number\n", operands[choice])
+	//For loop to get the values to calculate on from the user
+	for i := 0; i < opscount; i++ {
+		values = append(values, 0)
+		fmt.Scan(&values[i])
+	}
+	return values
+}
+
+// Function that prints a small terminal menu where the different math operations are available
 func operationChooser() {
 	choice := 0
 	var opscount int
 	values := []float64{}
-	fmt.Println("Choose what operation to perform by inputing the corresponding number")
-	fmt.Print(`
+	fmt.Print(`Choose what operation to perform by inputing the corresponding number
 	1. Addition
 	2. Subtraction
 	3. Multiplication
@@ -46,27 +74,18 @@ func operationChooser() {
 	6. Power
 	-`)
 	fmt.Scan(&choice)
-
+	//Switch to choose the operation to perform
 	switch choice {
 	case 1:
-		clearTerminal()
-		fmt.Print(`
-		---Addition---
-		Please input the amount numbers you want to add together:`)
-		fmt.Scan(&opscount)
-		fmt.Print("Please input the numbers to add together, press enter after each number\n")
-		for i := 0; i < opscount; i++ {
-			values = append(values, 0)
-			fmt.Scan(&values[i])
-		}
+		values = getUserInput(choice)
 		fmt.Printf("\nThe sum of the input is: %f", addition(values...))
 	case 2:
 		clearTerminal()
 		fmt.Print(`
 		---Subtraction---
-		Please input the amount numbers you want to subtract:`)
+		Input the amount numbers you want to subtract:`)
 		fmt.Scan(&opscount)
-		fmt.Print("Please input the numbers to subtract, press enter after each number\n")
+		fmt.Print("Input the numbers to subtract, press enter after each number\n")
 		for i := 0; i < opscount; i++ {
 			values = append(values, 0)
 			fmt.Scan(&values[i])
@@ -87,12 +106,14 @@ func operationChooser() {
 	}
 }
 
+// Function that sends a command(name) to the terminal with possible arguments(args)
 func runCmd(name string, args ...string) {
 	cmd := exec.Command(name, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Run()
 }
 
+// Switch function that checks to see what OS the user is on to use the corresponding command and arguments for it
 func clearTerminal() {
 	switch runtime.GOOS {
 	case "darwin":

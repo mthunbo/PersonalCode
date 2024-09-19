904ac1c0b28c13e08f4c435c4a9b950b58a74902
mThunbo <mthunbov2@gmail.com>
2024-09-19 14:45:52 +0200

    finished the last of the file sorting script. work on an event automated version will begin soon.

--- Python/FileSorter/FileSorter.py
+++ Python/FileSorter/FileSorter.py
@@ -1,35 +1,35 @@
 import shutil
-from pathlib import Path
 import json
 
-# Function that takes a file to move and a destination folder as arguements, checks for destination folder if it isn't there it creates it and moves the file there
-def fileMover(file, folder):
-    try:
-        if not folder.exists():
-            folder.mkdir(parents=True, exist_ok=True)
-        shutil.move(file, folder)
-    except shutil.Error as e:
-        print(e)
+from pathlib import Path
 
-# Funtion that sorts through the files in the folder given as an arguement
-def folderSorter(path):
-    with open('config.json', encoding='utf-8') as conf:
-        cats = json.load(conf)
+class FileEventHandler():
+    # Method that takes a file to move and a destination folder as arguements, checks for destination folder if it isn't there it creates it and moves the file there
+    def fileMover(file, folder):
+        try:
+            if not folder.exists():
+                folder.mkdir(parents=True, exist_ok=True)
+            shutil.move(file, folder)
+        except shutil.Error as e:
+            print(e)
 
-    #Creates a map with the names and extensions of the files to be able to sort them
-    extentionMap = {}
-    for cat in cats:
-        folderName = cat['name']
-        for extention in cat['extensions']:
-            extentionMap[extention] = folderName
+    # Method that sorts through the files in the folder given as an arguement
+    def folderSorter(path):
+        with open('config.json', encoding='utf-8') as conf:
+            cats = json.load(conf)
 
-    for file in path.iterdir():
-        if file.is_file() and not file.name.startswith('.'):
-            destFolder = extentionMap.get(file.suffix, 'Other')
-            fileMover(file, file.parent.joinpath(destFolder))
+        #Creates a map with the names and extensions of the files to be able to sort them
+        extentionMap = {}
+        for cat in cats:
+            folderName = cat['name']
+            for extention in cat['extensions']:
+                extentionMap[extention] = folderName
+
+        for file in path.iterdir():
+            if file.is_file() and not file.name.startswith('.'):
+                destFolder = extentionMap.get(file.suffix, 'Other')
+                FileEventHandler.fileMover(file, file.parent.joinpath(destFolder))
 
 if __name__ == '__main__':
-    homeFolder = str(Path.home())
-    downloadFolder = Path(f'{homeFolder}/Downloads')
-
-    folderSorter(downloadFolder)
+    downloadFolder = Path(f'{str(Path.home())}/Downloads')
+    FileEventHandler.folderSorter(downloadFolder)
